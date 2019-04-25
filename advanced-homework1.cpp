881024259e69575ef85bedb4c4cb4c9dd766beef
yuwen4514 <48576822+yuwen4514@users.noreply.github.com>
2019-04-26 02:38:26 +0800

    Add files via upload

---
+++ advanced-homework1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main() 
+{
+  char x; 
+  for (x=65; x<=90; x++)  
+  {
+	printf("%c ",x);
+  }
+	printf("\n");  
+	for (x=97; x<=122; x++)  
+	{
+		printf("%c ",x);
+	}
+	printf("\n");
+system ("pause"); 
+return 0;  
+}
